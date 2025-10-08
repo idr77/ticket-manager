443752611b0a85bd6171fe0ff4cd36900506c257
Eric Diallo <diallo.eric@gmail.com>
2025-10-09 01:24:07 +0200

    Fixup Angular frontend : NotFound component

--- frontend-angular/src/app/app-routing.module.ts
+++ frontend-angular/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './pages/home/home.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { NotFoundComponent } from './pages/not-found/not-found..ts';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
